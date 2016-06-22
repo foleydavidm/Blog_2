bb826aa7cec4c2439ec502c68009c2a38d3bf44a
steve <0x7374657465@gmail.com>
2016-06-22 12:27:47 -0400

    .gitignore removed, lots of files un-ignored.

---
+++ out/test/Blog2/step_definitions/common.rb
@@ -0,0 +1,7 @@
+When(/^I visit the blog for my favorite blogger$/) do
+  pending
+end
+
+When(/^I choose a blog post$/) do
+  pending
+end
---
+++ out/test/Blog2/step_definitions/creating_blog_entry_steps.rb
@@ -0,0 +1,15 @@
+Given(/^I am logged in as a blogger$/) do
+  pending
+end
+
+When(/^I publish a new blog post$/) do
+  pending
+end
+
+Then(/^I am notified that the blog post was successfully added$/) do
+  pending
+end
+
+And(/^the newly added blog post is at the top of the recent posts list$/) do
+  pending
+end
